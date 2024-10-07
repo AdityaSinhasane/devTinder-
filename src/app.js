882d541996a73d9ff06eac0e7c01e5cc27b935f9
AdityaSinhasane <adityasinhasane100@gmail.com>
2024-10-07 23:36:53 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,11 +2,30 @@ const express = require('express');
 
 const app = express();  // Here you are creating an instance of the express app. [OR] I am creating a new express.js application.
 
-//This will only handle GET call to  /user.
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params); //This is how you read the query parameters.
-    res.send({firstName:"Aditya",lastName:"Sinhasane"});
-}); 
+
+//app.use("/route",rH, [rH2, rH3], rH4, rH4);
+
+app.use("/user",[(req,res,next)=>{
+    // Route Handler 1
+    console.log("Handling the route user 1!!!");
+    next();
+    },
+    (req,res,next)=>{
+    // Route Handler 2
+    console.log("Handling the route user 2!!!");
+    next();
+    },
+    (req,res,next)=>{
+    // Route Handler 3
+    console.log("Handling the route user 3!!!");
+    next();
+    },
+    (req,res)=>{
+    // Route Handler 4
+    console.log("Handling the route user 4!!!");
+    res.send("4th Response!!");
+    }
+]); 
 
 app.listen(7777,()=>{
     console.log("Server is Successfully Listening On Port 7777....");
